84f88ff2aecf46481c8a2595c90b2842908bbf6a
Michalis Charatzoglou <micharatz97@gmail.com>
2023-12-18 19:06:29 +0200

    add padding to inputs

--- frontend/src/compoments/IndividualFormFields.jsx
+++ frontend/src/compoments/IndividualFormFields.jsx
@@ -7,9 +7,9 @@ const inputClass =
 function IndividualFormFields({ formData, handleInputChange }) {
 	return (
 		<>
-			<p className="pt-4 pr-2 text-gray-400">Personal Information</p>
+			<p className="pt-4 text-gray-400">Personal Information</p>
 			<div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-5">
-				<div>
+				<div className="p-2">
 					<label
 						htmlFor="fullname"
 						className="block mb-2 text-sm font-medium text-gray-900"
@@ -26,7 +26,7 @@ function IndividualFormFields({ formData, handleInputChange }) {
 						required
 					/>
 				</div>
-				<div>
+				<div className="p-2">
 					<label
 						htmlFor="email"
 						className="block mb-2 text-sm font-medium text-gray-900"
@@ -40,7 +40,7 @@ function IndividualFormFields({ formData, handleInputChange }) {
 						onDateChange={handleInputChange}
 					/>
 				</div>
-				<div>
+				<div className="p-2">
 					<label
 						htmlFor="age"
 						className="block mb-2 text-sm font-medium text-gray-900"
@@ -57,9 +57,105 @@ function IndividualFormFields({ formData, handleInputChange }) {
 					/>
 				</div>
 			</div>
-			<p className="pt-4 pr-2 text-gray-400">Details</p>
+			<p className="pt-4 pr-2 text-gray-400">House Details</p>
+
 			<div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-5">
-				<div>
+				<div className="p-2">
+					<label
+						htmlFor="houseType"
+						className="block mb-2 text-sm font-medium text-gray-900"
+					>
+						House type
+					</label>
+					<select
+						id="houseType"
+						name="houseType"
+						className={inputClass}
+						value={formData.houseType}
+					>
+						<option disabled selected>
+							Choose the house type
+						</option>
+						<option value="Apartment">Apartment </option>
+						<option value="Detached house">Detached house</option>
+					</select>
+				</div>
+				<div className="p-2">
+					<label
+						htmlFor="squareMeters"
+						className="block mb-2 text-sm font-medium text-gray-900"
+					>
+						Square Meters (&#13217;)
+					</label>
+					<select
+						id="squareMeters"
+						name="squareMeters"
+						className={inputClass}
+						value={formData.squareMeters}
+					>
+						<option disabled selected>
+							Choose the square meters
+						</option>
+						<option value="5-30">5-30</option>
+						<option value="31-65">31-65</option>
+						<option value="66-90">66-90</option>
+						<option value="90-120">90-120</option>
+						<option value="120+">120+</option>
+					</select>
+				</div>
+				<div className="p-2">
+					<label
+						htmlFor="floor"
+						className="block mb-2 text-sm font-medium text-gray-900"
+					>
+						What floor do you live on
+					</label>
+					<select
+						id="floor"
+						name="floor"
+						className={inputClass}
+						value={formData.floor}
+					>
+						<option disabled selected>
+							Choose the floor
+						</option>
+						<option value="Ground floor">Ground floor</option>
+						<option value="1st floor (building without pilot)">
+							1st floor (building without pilot)
+						</option>
+						<option value="1st floor (with pilot)">
+							1st floor (building with pilot)
+						</option>
+						<option value="On an intermediate floor">
+							On an intermediate floor
+						</option>
+						<option value="On the top floor">On the top floor</option>
+					</select>
+				</div>
+                <div className="p-2">
+					<label
+						htmlFor="houseBuilt"
+						className="block mb-2 text-sm font-medium text-gray-900"
+					>
+						When was your house built
+					</label>
+					<select
+						id="houseBuilt"
+						name="houseBuilt"
+						className={inputClass}
+						value={formData.houseBuilt}
+					>
+						<option disabled selected>
+							Choose when was your house built
+						</option>
+						<option value="Before 1980">Before 1980</option>
+						<option value="1980-2000">1980-2000</option>
+						<option value="2001-2010">2001-2010</option>
+						<option value="After 2010">After 2010</option>
+						<option value="Dont know">Dont know</option>
+					</select>
+				</div>
+				<div className="p-2">
 					<label
 						htmlFor="numberOfoccupants"
 						className="block mb-2 text-sm font-medium text-gray-900"
@@ -82,7 +178,7 @@ function IndividualFormFields({ formData, handleInputChange }) {
 						<option value="5+">5+</option>
 					</select>
 				</div>
-				<div>
+				<div className="p-2">
 					<label
 						htmlFor="typeOfoccupants"
 						className="block mb-2 text-sm font-medium text-gray-900"
@@ -113,50 +209,7 @@ function IndividualFormFields({ formData, handleInputChange }) {
 						<option value="Adults aged 65+ only">Adults aged 65+ only</option>
 					</select>
 				</div>
-				<div>
-					<label
-						htmlFor="houseType"
-						className="block mb-2 text-sm font-medium text-gray-900"
-					>
-						House type
-					</label>
-					<select
-						id="houseType"
-						name="houseType"
-						className={inputClass}
-						value={formData.houseType}
-					>
-						<option disabled selected>
-							Choose the house type
-						</option>
-						<option value="Apartment">Apartment </option>
-						<option value="Detached house">Detached house</option>
-					</select>
-				</div>
-				<div>
-					<label
-						htmlFor="squareMeters"
-						className="block mb-2 text-sm font-medium text-gray-900"
-					>
-						Square Meters (&#13217;)
-					</label>
-					<select
-						id="squareMeters"
-						name="squareMeters"
-						className={inputClass}
-						value={formData.squareMeters}
-					>
-						<option disabled selected>
-							Choose the square meters
-						</option>
-						<option value="5-30">5-30</option>
-						<option value="31-65">31-65</option>
-						<option value="66-90">66-90</option>
-						<option value="90-120">90-120</option>
-						<option value="120+">120+</option>
-					</select>
-				</div>
-				<div>
+				<div className="p-2">
 					<label
 						htmlFor="ageElectricityManager"
 						className="block mb-2 text-sm font-medium text-gray-900"
@@ -180,59 +233,7 @@ function IndividualFormFields({ formData, handleInputChange }) {
 						<option value="65+">65+</option>
 					</select>
 				</div>
-				<div>
-					<label
-						htmlFor="floor"
-						className="block mb-2 text-sm font-medium text-gray-900"
-					>
-						What floor do you live on
-					</label>
-					<select
-						id="floor"
-						name="floor"
-						className={inputClass}
-						value={formData.floor}
-					>
-						<option disabled selected>
-							Choose the floor
-						</option>
-						<option value="Ground floor">Ground floor</option>
-						<option value="1st floor (building without pilot)">
-							1st floor (building without pilot)
-						</option>
-						<option value="1st floor (with pilot)">
-							1st floor (building with pilot)
-						</option>
-						<option value="On an intermediate floor">
-							On an intermediate floor
-						</option>
-						<option value="On the top floor">On the top floor</option>
-					</select>
-				</div>
-				<div>
-					<label
-						htmlFor="houseBuilt"
-						className="block mb-2 text-sm font-medium text-gray-900"
-					>
-						When was your house built
-					</label>
-					<select
-						id="houseBuilt"
-						name="houseBuilt"
-						className={inputClass}
-						value={formData.houseBuilt}
-					>
-						<option disabled selected>
-							Choose when was your house built
-						</option>
-						<option value="Before 1980">Before 1980</option>
-						<option value="1980-2000">1980-2000</option>
-						<option value="2001-2010">2001-2010</option>
-						<option value="After 2010">After 2010</option>
-						<option value="Dont know">Dont know</option>
-					</select>
-				</div>
-				<div>
+				<div className="p-2">
 					<label
 						htmlFor="frames"
 						className="block mb-2 text-sm font-medium text-gray-900"
@@ -260,7 +261,7 @@ function IndividualFormFields({ formData, handleInputChange }) {
 						<option value="Dont know">Dont know</option>
 					</select>
 				</div>
-				<div>
+				<div className="p-2">
 					<label
 						htmlFor="heatingType"
 						className="block mb-2 text-sm font-medium text-gray-900"
@@ -278,7 +279,7 @@ function IndividualFormFields({ formData, handleInputChange }) {
 						</option>
 					</select>
 				</div>
-				<div>
+				<div className="p-2">
 					<label
 						htmlFor="haveSolarPanels"
 						className="block mb-2 text-sm font-medium text-gray-900"
@@ -298,7 +299,7 @@ function IndividualFormFields({ formData, handleInputChange }) {
 						<option value={false}>No</option>
 					</select>
 				</div>
-				<div>
+				<div className="p-2">
 					<label
 						htmlFor="hotWater"
 						className="block mb-2 text-sm font-medium text-gray-900"
@@ -316,7 +317,7 @@ function IndividualFormFields({ formData, handleInputChange }) {
 						</option>
 					</select>
 				</div>
-				<div>
+				<div className="p-2">
 					<label
 						htmlFor="evCarCharger"
 						className="block mb-2 text-sm font-medium text-gray-900"
