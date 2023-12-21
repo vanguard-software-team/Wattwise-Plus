314b9eedda8a756de85daf45da521e3284f260ee
Michalis Charatzoglou <micharatz97@gmail.com>
2023-12-21 12:52:41 +0200

    individual finished

--- frontend/src/compoments/CompanyInputFields.jsx
+++ frontend/src/compoments/CompanyInputFields.jsx
@@ -1,12 +1,14 @@
 import { Field, ErrorMessage } from "formik";
 
+
 const inputClass =
 	"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-300 focus:border-orange-300 block w-72 p-2.5";
+
 
 function CompanyInputFields() {
 	return (
 		<>
-			<p className="pt-4 text-gray-400">Personal Information</p>
+			<p className="pt-4 text-gray-400">Company Information</p>
 			<div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-5">
 				{/* Company Name */}
 				<div className="p-2">
@@ -14,13 +16,13 @@ function CompanyInputFields() {
 						htmlFor="companyName"
 						className="block mb-2 text-sm font-medium text-gray-900"
 					>
-						Company Name
+						Full Name
 					</label>
 					<Field
 						name="companyName"
 						type="text"
-						placeholder="Type your company name"
 						className={inputClass}
+						placeholder="Type the company name"
 					/>
 					<ErrorMessage name="companyName" component="div" />
 				</div>
--- frontend/src/compoments/FormMoreInformation.jsx
+++ frontend/src/compoments/FormMoreInformation.jsx
@@ -1,179 +1,176 @@
 import { useState, useEffect } from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 import ConfirmationModal from "./ComfirmationModal";
 import CompanyInputFields from "./CompanyInputFields";
-import IndividualFormFields from "./IndividualFormFields";
+import IndividualInputFields from "./IndividualInputFields";
 
 const inputClass =
 	"bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-orange-300 focus:border-orange-300 block w-72 p-2.5";
 
+const individualSchema = Yup.object().shape({
+	fullName: Yup.string(),
+	birthDate: Yup.date(),
+	phoneNumber: Yup.string(),
+	numberOfoccupants: Yup.string(),
+	houseType: Yup.string(),
+	squareMeters: Yup.string(),
+	typeOfoccupants: Yup.string(),
+	ageElectricityManager: Yup.string(),
+	floor: Yup.string(),
+	houseBuilt: Yup.string(),
+	frames: Yup.string(),
+	heatingType: Yup.string(),
+	haveSolarPanels: Yup.bool(),
+	hotWater: Yup.string(),
+	evCarCharger: Yup.bool(),
+});
+
+const predefinedIndividual = {
+	fullName: "John Doe",
+	birthDate: new Date("2019-01-01"),
+	phoneNumber: "1234567890",
+	numberOfoccupants: "4",
+	houseType: "Apartment",
+	squareMeters: "31-65",
+	typeOfoccupants: "Adults aged 18 to 29 only",
+	ageElectricityManager: "25-34",
+	floor: "1st floor (building without pilot)",
+	houseBuilt: "1980-2000",
+	frames: "Wooden or metal with single glazing",
+	heatingType: "Central gas boiler",
+	haveSolarPanels: true,
+	hotWater: "Gas boiler",
+	evCarCharger: false,
+};
+
+const companySchema = Yup.object().shape({
+	companyName: Yup.string(),
+	TIN: Yup.string(),
+	phoneNumber: Yup.string(),
+	numberOfEmployees: Yup.string(),
+	
+});
+
+const predefinedCompany = {
+	companyName: "The boaring company",
+	registrationNumber: "12313131314",
+	address: "This is address",
+};
+
 function FormMoreInformation() {
-	const initialFormData = {
-		// Individual fields
-		fullName: "",
-		birthDate: undefined,
-		phoneNumber: "",
-		numberOfoccupants: undefined,
-		houseType: undefined,
-		squareMeters: undefined,
-		typeOfoccupants: undefined,
-		ageElectricityManager: undefined,
-		floor: undefined,
-		houseBuilt: undefined,
-		frames: undefined,
-		heatingType: undefined,
-		haveSolarPanels: undefined,
-		hotWater: undefined,
-		evCarCharger: undefined,
-		// Company fields
-		companyName: undefined,
-		registrationNumber: undefined,
-		address: undefined,
-	};
 	const [type, setType] = useState("Individual");
-	const [formData, setFormData] = useState({ ...initialFormData });
-	const [isChanged, setIsChanged] = useState(false);
 	const [showModal, setShowModal] = useState(false);
 	const [pendingType, setPendingType] = useState(null);
+	const [initialValues, setInitialValues] = useState(predefinedIndividual); // Add this state
 
 	useEffect(() => {
-		setIsChanged(JSON.stringify(formData) !== JSON.stringify(initialFormData));
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [formData]);
-
-	const isFormDataFilled = () => {
+		// Update initial values when type changes
 		if (type === "Individual") {
-			return (
-				formData.fullName ||
-				formData.birthDate ||
-				formData.phoneNumber ||
-				formData.numberOfoccupants ||
-				formData.houseType ||
-				formData.squareMeters ||
-				formData.typeOfoccupants ||
-				formData.ageElectricityManager ||
-				formData.floor ||
-				formData.houseBuilt ||
-				formData.frames ||
-				formData.heatingType ||
-				formData.haveSolarPanels ||
-				formData.hotWater ||
-				formData.evCarCharger
-			);
+			setInitialValues(predefinedIndividual);
 		} else {
-			// Company fields check
-			return (
-				formData.companyName || formData.registrationNumber || formData.address
-			);
+			setInitialValues(predefinedCompany);
 		}
-	};
+	}, [type]);
 
-	const handleTypeChange = (newType) => {
-		if (type !== newType && isFormDataFilled()) {
+	const handleTypeChange = (newType, resetForm) => {
+		if (type !== newType) {
 			setPendingType(newType);
 			setShowModal(true);
 		} else {
 			setType(newType);
+			resetForm();
 		}
 	};
 
-	const resetFormData = () => {
-		setFormData({ ...initialFormData });
-	};
-
-	const handleInputChange = (e) => {
-		setFormData({ ...formData, [e.target.name]: e.target.value });
-	};
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		let submittedData = {};
-		if (type === "Individual") {
-			submittedData = {
-				fullName: formData.fullName,
-				birthDate: formData.birthDate,
-				phoneNumber: formData.phoneNumber,
-				numberOfoccupants: formData.numberOfoccupants,
-				houseType: formData.houseType,
-				squareMeters: formData.squareMeters,
-				typeOfoccupants: formData.typeOfoccupants,
-				ageElectricityManager: formData.ageElectricityManager,
-				floor: formData.floor,
-				houseBuilt: formData.houseBuilt,
-				frames: formData.frames,
-				heatingType: formData.heatingType,
-				haveSolarPanels: formData.haveSolarPanels,
-				hotWater: formData.hotWater,
-				evCarCharger: formData.evCarCharger,
-			};
-		} else if (type === "Company") {
-			submittedData = {
-				companyName: formData.companyName,
-				registrationNumber: formData.registrationNumber,
-				address: formData.address,
-			};
-		}
-		console.log(submittedData);
-		resetFormData();
-	};
-
-	const handleModalConfirm = () => {
+	const handleModalConfirm = (resetForm) => {
 		if (pendingType !== null) {
 			setType(pendingType);
 			setPendingType(null);
+			resetForm({
+				values:
+					pendingType === "Individual"
+						? predefinedIndividual
+						: predefinedCompany,
+			});
 		}
-		resetFormData();
 		setShowModal(false);
 	};
 
+	const handleSubmit = (values, { setSubmitting, resetForm }) => {
+		console.log(values);
+		// handle proper here after take the response from the backend
+		// const handleSubmit = async (values, { setSubmitting, resetForm }) => {
+		// 	try {
+		// 		// Asynchronous operation (e.g., API call)
+		// 		await someAsyncOperation(values);
+
+		// 		// Reset the form to initial values after successful submission
+		// 		resetForm();
+		// 	} catch (error) {
+		// 		console.error("Error submitting form:", error);
+		// 	} finally {
+		// 		// Ensure isSubmitting is set to false after operation
+		// 		setSubmitting(false);
+		// 	}
+		// };
+		resetForm();
+		setSubmitting(false);
+	};
+
 	return (
-		<form onSubmit={handleSubmit}>
-			<div className="flex justify-center items-center pt-4">
-				<div>
-					<label className="mb-2 text-sm  text-gray-900 dark:text-gray-300">
-						Type
-					</label>
-					<select
-						name="type"
-						className={inputClass}
-						value={type}
-						onChange={(e) => handleTypeChange(e.target.value)}
-					>
-						<option value="Individual">Individual</option>
-						<option value="Company">Company</option>
-					</select>
-				</div>
-			</div>
+		<Formik
+			initialValues={initialValues}
+			enableReinitialize // Add this property
+			validationSchema={
+				type === "Individual" ? individualSchema : companySchema
+			}
+			onSubmit={handleSubmit}
+		>
+			{({ resetForm, isSubmitting, dirty, isValid }) => (
+				<Form>
+					<div className="flex justify-center items-center pt-4 ">
+						<div>
+							<label className="mb-2 text-sm text-gray-900 dark:text-gray-300">
+								Consumer Type
+							</label>
+							<Field
+								as="select"
+								name="type"
+								className={inputClass}
+								onChange={(e) => handleTypeChange(e.target.value, resetForm)}
+							>
+								<option value="Individual">Individual</option>
+								<option value="Company">Company</option>
+							</Field>
+						</div>
+					</div>
+					<hr className="mt-5"></hr>
 
-			{type === "Individual" ? (
-				<IndividualFormFields
-					formData={formData}
-					handleInputChange={handleInputChange}
-				/>
-			) : (
-				<CompanyInputFields
-					formData={formData}
-					handleInputChange={handleInputChange}
-				/>
+					{type === "Individual" ? (
+						<IndividualInputFields />
+					) : (
+						<CompanyInputFields />
+					)}
+
+					<div className="flex justify-center pb-5">
+						<button
+							type="submit"
+							className="mt-4 bg-orange-500 text-sm text-white p-2 rounded disabled:bg-orange-300"
+							disabled={isSubmitting || !dirty || !isValid}
+						>
+							Save changes
+						</button>
+						<ConfirmationModal
+							isOpen={showModal}
+							onClose={() => setShowModal(false)}
+							onConfirm={() => handleModalConfirm(resetForm)}
+							message="If you change the consumer type, the stored information will be reset. Are you sure?"
+						/>
+					</div>
+				</Form>
 			)}
-
-			<div className="flex justify-center pb-5">
-				<button
-					type="submit"
-					disabled={!isChanged}
-					className={`mt-4 bg-orange-500 text-sm text-white p-2 rounded disabled:bg-orange-300 ${
-						!isChanged ? "opacity-50 cursor-not-allowed" : ""
-					}`}
-				>
-					Save changes
-				</button>
-				<ConfirmationModal
-					isOpen={showModal}
-					onClose={() => setShowModal(false)}
-					onConfirm={handleModalConfirm}
-					message="If you change the consumer type, the stored  information will be reset. Are you sure?"
-				/>
-			</div>
-		</form>
+		</Formik>
 	);
 }
 
