ff4bd98d76ec3ed04f4db63f63d5f9df445bce42
Vasilis Stergiou <vstergiou01@gmail.com>
2025-08-23 20:12:33 +0300

    refactor: removed redundunt comment
    refs: #14

--- frontend/src/authenticated/consumer/Insights.jsx
+++ frontend/src/authenticated/consumer/Insights.jsx
@@ -352,7 +352,7 @@ function Insights() {
 
     fetchData();
   }, [dateRangesComparison, defaultButtonNameComparison, userEmail]);
-  console.log("default :", defaultComparisonMetrics);
+
   return (
     <AuthenticatedLayout>
       <div className='p-1 sm:ml-40 bg-gray-200 font-ubuntu'>
