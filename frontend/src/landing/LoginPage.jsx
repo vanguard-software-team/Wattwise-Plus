78d6a1fbd9900039af45ecad638f8788d840694a
Vasilis Stergiou <vstergiou01@gmail.com>
2025-08-24 18:19:15 +0300

    refactor: removed  unecessary footer from landing, login and sign up page
    refs: #14

--- frontend/src/landing/LoginPage.jsx
+++ frontend/src/landing/LoginPage.jsx
@@ -1,17 +1,14 @@
 import Navbar from "../components/Navbar.jsx";
-import Footer from "../components/Footer.jsx";
 import LoginForm from "../components/LoginForm.jsx";
 function LoginPage() {
-	return (
-		<>
-			<Navbar />
-			<div className="bg-gradient-to-b from-slate-100 to-orange-200 flex justify-center items-center pb-60 h-screen">
-				<LoginForm />
-			</div>
-
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <div className='bg-gradient-to-b from-slate-100 to-orange-200 flex justify-center items-center pb-60 h-screen'>
+        <LoginForm />
+      </div>
+    </>
+  );
 }
 
 export default LoginPage;
